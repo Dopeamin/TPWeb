34e8326c50a546653bc0df53500315dcc9247d3d
Med Amine Hamdouni <hamdouni.medamin@gmail.com>
2021-03-13 10:55:33 +0100

    fixed bug

--- TP1/Ex1/Med Amine Hamdouni/Version Prompt/main.js
+++ TP1/Ex1/Med Amine Hamdouni/Version Prompt/main.js
@@ -1,31 +1,33 @@
-difficulty = 0;
-while (difficulty > 3 || difficulty < 1) {
-    difficulty = prompt("Enter Difficulty (1:easy,2:normal,3:hard)", "1");
-}
-tent = 8;
-if (difficulty == 1) {
+while (true) {
+    difficulty = 0;
+    while (difficulty > 3 || difficulty < 1) {
+        difficulty = prompt("Enter Difficulty (1:easy,2:normal,3:hard)", "1");
+    }
     tent = 8;
-} else if (difficulty == 2) {
-    tent = 5;
-} else {
-    tent = 3;
-}
-random = Math.floor(Math.random() * 11);
-alert("You have " + tent + " tries");
-while (tent > 0) {
-    do {
-        tentative = prompt("Choose a number from 0 to 10", "0");
-    } while (tentative > 10 || tentative < 0);
-    if (tentative == random) {
-        alert("You won");
-        break;
+    if (difficulty == 1) {
+        tent = 8;
+    } else if (difficulty == 2) {
+        tent = 5;
     } else {
-        tent--;
-        if (tent == 0) {
-            alert("You Lost");
+        tent = 3;
+    }
+    random = Math.floor(Math.random() * 11);
+    alert("You have " + tent + " tries");
+    while (tent > 0) {
+        do {
+            tentative = prompt("Choose a number from 0 to 10", "0");
+        } while (tentative > 10 || tentative < 0);
+        if (tentative == random) {
+            alert("You won");
+            break;
         } else {
-            alert("Wrong number you have " + tent + " tries left")
-        }
+            tent--;
+            if (tent == 0) {
+                alert("You Lost");
+            } else {
+                alert("Wrong number you have " + tent + " tries left")
+            }
 
+        }
     }
 }
