3751e524155fbb09e950119058bc9fe9ca6c640d
Med Amine Hamdouni <hamdouni.medamin@gmail.com>
2021-04-18 18:32:29 +0100

    Added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2f77013a53882633b616838cd0f8df8bb6913a25',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2f77013a53882633b616838cd0f8df8bb6913a25',
+    ),
+  ),
+);
