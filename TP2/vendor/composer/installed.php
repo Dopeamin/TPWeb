2f77013a53882633b616838cd0f8df8bb6913a25
Med Amine Hamdouni <hamdouni.medamin@gmail.com>
2021-04-18 18:27:56 +0100

    Added composer

---
+++ TP2/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0591bb69d2ca0c76c90b079e175b6dc94c8a499f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0591bb69d2ca0c76c90b079e175b6dc94c8a499f',
+    ),
+  ),
+);
