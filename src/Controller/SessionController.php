56da73e6b94d6569706251193aa89acee34582d4
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-24 10:28:28 +0100

    Exercice Todo

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SessionController extends AbstractController
+{
+    /**
+     * @Route("/session", name="session")
+     */
+    public function index(SessionInterface $session): Response
+    {
+        if (!$session->has('user')){
+            $message = "Bienvenu chez nous :)";
+            $this->addFlash('welcome', $message);
+            $session->set('user', 'newUser');
+        }
+        return $this->render('session/index.html.twig');
+    }
+}
