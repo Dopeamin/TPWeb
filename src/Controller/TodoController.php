de5f5d02c9f6b26901f52758a6cf45dbee3adc5d
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-04-29 14:32:17 +0100

    TP3

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Class TodoController
@@ -19,7 +20,7 @@ class TodoController extends AbstractController
      */
     public function index(SessionInterface $session): Response
     {
-        if(! $session->has('todos')) {
+        if(! $session->has('todos') || sizeof($session->get('todos'))==0) {
             $todos = [
                 'lundi' => 'HTML',
                 'mardi' => 'CSs',
@@ -32,10 +33,11 @@ class TodoController extends AbstractController
     }
 
     /**
-     * @Route("/add/{name}/{content<\d+>}", name="addTodo")
+     * @Route("/add", name="addTodo")
      */
-    public function addTodo($name, $content, SessionInterface $session) {
-
+    public function addTodo(SessionInterface $session,Request $request) {
+        $name = $request->query->get('name');
+        $content = $request->query->get('content');
         // Vérifier que ma session contient le tableau de todo
         if (!$session->has('todos')) {
             //ko => messsage erreur + redirection
@@ -54,6 +56,56 @@ class TodoController extends AbstractController
                 $this->addFlash('success', "Le todo $name a été ajouté avec succès");
             }
         }
+        return $this->redirectToRoute('todo');
+    }
+    /**
+     * @Route("/delete/{name}", name="deleteTodo")
+     */
+    public function deleteTodo($name, SessionInterface $session){
+        if (!$session->has('todos')) {
+            //ko => messsage erreur + redirection
+            $this->addFlash('error', "La liste des todos n'est pas encore initialisée");
+        } else {
+            //ok
+            // Je vérifie si le todo existe
+            $todos = $session->get('todos');
+                //ko => messsage erreur + redirection
+                unset($todos[$name]);
+                $session->set('todos',$todos);
+                $this->addFlash('error', "Le todo $name est supprimé avec succés");
+            
+        }
+        return $this->redirectToRoute('todo');
+    }
+    /**
+     * @Route("/modify/{name}/{content}", name="modifyTodo")
+     */
+    public function modifyTodo($name,$content, SessionInterface $session){
+        if (!$session->has('todos')) {
+            //ko => messsage erreur + redirection
+            $this->addFlash('error', "La liste des todos n'est pas encore initialisée");
+        } else {
+            //ok
+            // Je vérifie si le todo existe
+            $todos = $session->get('todos');
+            if (isset($todos[$name]) && (sizeof($todos)>0)) {
+                //ko => messsage erreur + redirection
+                $todos[$name] = $content;
+                $session->set('todos',$todos);
+                $this->addFlash('error', "Le todo $name est modifié avec succés");
+            } else {
+                //ok => j ajoute et je redirige avec message succès
+                
+                $this->addFlash('error', "Le todo $name n'existe pas");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+    /**
+     * @Route("/clear", name="clear")
+     */
+    public function clear(SessionInterface $session){
+        $session->clear('todos');
         return $this->redirectToRoute('todo');
     }
 }
